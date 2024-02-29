53ce0cd83bbe99dfbf9cc31ca61818a5f9e38bcc
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-02-29 13:07:03 -0600

    Add email verification message to AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,12 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verificar cuenta')
+                ->line('Por favor, haga clic en el siguiente enlace para verificar su cuenta.')
+                ->action('Verificar cuenta', $url)
+                ->line('Si no creó una cuenta, no es necesario realizar ninguna acción.');
+        });
     }
 }
