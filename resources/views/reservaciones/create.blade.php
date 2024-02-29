c9bad39d014d8cbeb69a46dee5c97c441ddf337f
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-02-29 14:38:34 -0600

    Add livewire/livewire dependency and update views

--- resources/views/reservaciones/create.blade.php
+++ resources/views/reservaciones/create.blade.php
@@ -9,7 +9,10 @@
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
           <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 text-gray-900">
-                  Formulario de solicitud de reservación
+                <h1 class="text-3xl font-bold text-center mb-10">Solicitar Sala a Reservar</h1>
+                  <div class="md:flex md:justify-center p-5">
+                    <livewire:solicitar-sala />
+                  </div>
               </div>
           </div>
       </div>
