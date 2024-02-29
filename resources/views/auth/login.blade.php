d8ac20da0fc04e892d511581d0d1a5b24ec58ea5
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-02-28 23:48:08 -0600

    Update login page with translated labels and error messages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,18 +4,18 @@
 
     <form method="POST" action="{{ route('login') }}" novalidate>
         @csrf
-        <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        <x-input-error :messages="str_replace('email', 'correo electrónico', $errors->get('email'))" class="mt-2" />
+        <x-input-error :messages="str_replace('password', 'contraseña', $errors->get('password'))" class="mt-2" />
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Correo Eléctronico')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Contraseña')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -32,14 +32,12 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
+            <x-link>
+                Olvidaste tu contraseña?
+            </x-link>    
 
             <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+                {{ __('Inicar Sesión') }}
             </x-primary-button>
         </div>
     </form>
