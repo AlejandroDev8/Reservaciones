e4f5ff4c7889ef4fdda8ef89eb0a4c9946a45c07
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-03-03 23:44:14 -0600

    Refactor navigation and display of reservations

--- resources/views/livewire/mostrar-solicitudes.blade.php
+++ resources/views/livewire/mostrar-solicitudes.blade.php
@@ -2,16 +2,18 @@
   @if (count($solicitudes) > 0)
   @foreach ($solicitudes as $solicitud)
   <div class="p-6 text-gray-900 bg-white md:flex justify-between items-center">
-    <div class="leading-10">
-      <a href="#" class="text-xl font-bold">
+    <div class="leading-10 space-y-3">
+      <h2 class="text-xl font-bold">
         @if ($solicitud->sala)
-        Sala Solicitada: {{ $solicitud->sala->salas }}
+        Sala Solicitada: <span class="text-indigo-600 normal-case font-normal">{{ $solicitud->sala->salas }}</span>
         @else
         Sin sala asociada
         @endif
-      </a>
-      <p class="text-sm text-gray-600 font-bold">{{ $solicitud->email }}</p>
-      <p class="text-sm text-gray-500">Fecha de Reservación: {{ $solicitud->fecha->format('d/m/Y') }} </p>
+      </h2>
+      <p class="text-sm text-gray-600 font-bold">Correo electrónico asociado: <span
+          class="text-indigo-600 normal-case">{{ $solicitud->email }}</span></p>
+      <p class="text-sm text-gray-500 font-bold">Fecha de Reservación: <span class="text-indigo-600 normal-case">{{
+          $solicitud->fecha->format('d/m/Y') }}</span> </p>
     </div>
     <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
       <a href="{{route('reservaciones.edit', $solicitud->id)}}"
