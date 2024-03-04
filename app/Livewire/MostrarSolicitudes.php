ac07f8cc76081823d4010d50ca39a9e69d84f736
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-03-03 22:35:33 -0600

    Refactor mostrar-solicitudes component and add eliminarSolicitud method

--- app/Livewire/MostrarSolicitudes.php
+++ app/Livewire/MostrarSolicitudes.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire;
 
-use App\Models\Reservacion;
 use App\Models\Sala;
 use Livewire\Component;
+use App\Models\Reservacion;
+use Livewire\Attributes\On;
 
 class MostrarSolicitudes extends Component
 {
+    protected $listeners = ['eliminarSolicitud'];
+
+    public function eliminarSolicitud(Reservacion $reservacion)
+    {
+        $reservacion->delete();
+    }
+
     public function render()
     {
         $solicitudes = Reservacion::where('user_id', auth()->user()->id)->paginate(10);
--- resources/views/livewire/mostrar-solicitudes.blade.php
+++ resources/views/livewire/mostrar-solicitudes.blade.php
@@ -1,59 +1,64 @@
 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-    @if (count($solicitudes) > 0)
-    @foreach ($solicitudes as $solicitud)
-    <div class="p-6 text-gray-900 bg-white md:flex justify-between items-center">
-        <div class="leading-10">
-            <a href="#" class="text-xl font-bold">
-                @if ($solicitud->sala)
-                Sala Solicitada: {{ $solicitud->sala->salas }}
-                @else
-                Sin sala asociada
-                @endif
-            </a>
-            <p class="text-sm text-gray-600 font-bold">{{ $solicitud->email }}</p>
-            <p class="text-sm text-gray-500">Fecha de Reservación: {{ $solicitud->fecha->format('d/m/Y') }} </p>
-        </div>
-        <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
-            <a href="{{route('reservaciones.edit', $solicitud->id)}}"
-                class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-                Editar
-            </a>
-            <a href="#" class="bg-red-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
-                Eliminar
-            </a>
-        </div>
+  @if (count($solicitudes) > 0)
+  @foreach ($solicitudes as $solicitud)
+  <div class="p-6 text-gray-900 bg-white md:flex justify-between items-center">
+    <div class="leading-10">
+      <a href="#" class="text-xl font-bold">
+        @if ($solicitud->sala)
+        Sala Solicitada: {{ $solicitud->sala->salas }}
+        @else
+        Sin sala asociada
+        @endif
+      </a>
+      <p class="text-sm text-gray-600 font-bold">{{ $solicitud->email }}</p>
+      <p class="text-sm text-gray-500">Fecha de Reservación: {{ $solicitud->fecha->format('d/m/Y') }} </p>
     </div>
-    @endforeach
-    @else
-    <div class="p-6 text-gray-900 bg-white">
-        <p class="text-xl font-bold">No tienes solicitudes actualmente</p>
+    <div class="flex flex-col md:flex-row items-stretch gap-3 mt-5 md:mt-0">
+      <a href="{{route('reservaciones.edit', $solicitud->id)}}"
+        class="bg-blue-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+        Editar
+      </a>
+      <button wire:click="$dispatch('mostrarAlerta', {{ $solicitud->id }})"
+        class="bg-red-800 py-2 px-4 rounded-lg text-white text-xs font-bold uppercase text-center">
+        Eliminar
+      </button>
     </div>
-    @endif
-    <div class="mt-10">
-        {{ $solicitudes->links() }}
-    </div>
+  </div>
+  @endforeach
+  @else
+  <div class="p-6 text-gray-900 bg-white">
+    <p class="text-xl font-bold">No tienes solicitudes actualmente</p>
+  </div>
+  @endif
+  <div class="mt-10">
+    {{ $solicitudes->links() }}
+  </div>
 </div>
 
 @push('scripts')
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
+  Livewire.on('mostrarAlerta', solicitudId => {
     Swal.fire({
-  title: '¿Estás seguro de eliminar la solicitud?',
-  text: "No podrás revertir esto!",
-  icon: 'warning',
-  showCancelButton: true,
-  confirmButtonColor: '#3085d6',
-  cancelButtonColor: '#d33',
-  confirmButtonText: 'Sí, eliminar!',
-    cancelButtonText: 'Cancelar'
-}).then((result) => {
-  if (result.isConfirmed) {
-    Swal.fire(
-      'Solicitud eliminada correctamente!',
-        'Tu solicitud ha sido eliminada.',
-      'success'
-    )
-  }
-})
+    title: '¿Estás seguro de eliminar la solicitud?',
+    text: "No podrás revertir esto!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sí, eliminar!',
+      cancelButtonText: 'Cancelar'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      @this.call('eliminarSolicitud', solicitudId)
+      Swal.fire(
+        'Solicitud eliminada correctamente!',
+          'Tu solicitud ha sido eliminada.',
+        'success'
+      )
+    }
+  })
+  })
+
 </script>
 @endpush
