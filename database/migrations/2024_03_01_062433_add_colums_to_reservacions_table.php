d9ad9d18176f27adcfbea590ffdfbd36411344a4
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-03-04 19:40:58 -0600

    Update columns in reservations table and fix typo in welcome.blade.php

--- database/migrations/2024_03_01_062433_add_colums_to_reservacions_table.php
+++ database/migrations/2024_03_01_062433_add_colums_to_reservacions_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->date('fecha')->unique();
             $table->foreignId('acomodo_id')->constrained()->onDelete('cascade');
             $table->text('extras')->nullable();
-            $table->integer('aceptado')->default(1); // 1 = aceptado, 0 = rechazado
+            $table->integer('estado')->default(0); // 0 = pendiente, 1 = aceptado, 2 = rechazado
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
         });
     }
@@ -31,7 +31,7 @@ return new class extends Migration
             $table->dropForeign(['sala_id']);
             $table->dropForeign(['acomodo_id']);
             $table->dropForeign(['user_id']);
-            $table->dropColumn(['email', 'sala_id', 'fecha', 'acomodo_id', 'extras', 'aceptado', 'user_id']);
+            $table->dropColumn(['email', 'sala_id', 'fecha', 'acomodo_id', 'extras', 'estado', 'user_id']);
         });
     }
 };
