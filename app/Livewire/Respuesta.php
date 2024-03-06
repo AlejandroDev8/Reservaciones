0b9400a7a1d02c7709e446bbf608bd57ad177852
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2024-03-06 17:03:54 -0600

    Refactor Livewire component and update view

--- app/Livewire/Respuesta.php
+++ app/Livewire/Respuesta.php
@@ -2,19 +2,39 @@
 
 namespace App\Livewire;
 
-use App\Models\Acomodo;
 use App\Models\Sala;
+use App\Models\Acomodo;
 use Livewire\Component;
+use App\Models\Reservacion;
+use Illuminate\Support\Carbon;
 
 class Respuesta extends Component
 {
-    public function render()
+    public $reservacion_id;
+    public $email;
+    public $sala;
+    public $fecha;
+    public $acomodo;
+    public $extras;
+
+    public function mount(Reservacion $reservacion)
+    {
+        $this->reservacion_id = $reservacion->id;
+        $this->email = $reservacion->email;
+        $this->sala = $reservacion->sala_id;
+        $this->fecha = Carbon::parse($reservacion->fecha)->format('Y-m-d');
+        $this->acomodo = $reservacion->acomodo_id;
+        $this->extras = $reservacion->extras;
+    }
+
+    public function render(Reservacion $reservacion)
     {
         $salas = Sala::all();
         $acomodos = Acomodo::all();
+
         return view('livewire.respuesta', [
             'salas' => $salas,
-            'acomodos' => $acomodos
+            'acomodos' => $acomodos,
         ]);
     }
 }
